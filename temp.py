b31cee2f154d90e3fff551ca3ee9effea3ca8e7e
Inaaa <chanchan.li@outlook.com>
2020-03-05 12:17:51 +0100

    add visulation

--- temp.py
+++ temp.py
@@ -15,43 +15,60 @@ matplotlib.use( 'tkagg' )
 cfg = two_side_config()
 path ='/home/chli/Documents/hiwi/Pricing_strategie/result/'
 
+model = BasisParameter(cfg,path)
+
+u = model.consumer()
+p = model.producer()
+V = model.profit(u, p)
+name =['n','u','p','V','u_p_v_n.png']
+model.visulation(name,u,p,V)
 
 
-n = np.arange(0, 16, 1)
-u = np.zeros(16)
-p = np.zeros(16)
-v = np.zeros(16)
-for i in n:
-    model = BasisParameter(i, cfg,path)
-    u[i] = model.consumer()
-    p[i] = model.producer()
-    v[i] = model.profit(u[i], p[i])
-print(n,'++++',u)
-plt.figure()
-plt.plot(n, u, color="r", linestyle="--", linewidth=1.0, label='u')
-plt.plot(n, p, color="g", linestyle="--", linewidth=1.0, label='p')
-plt.plot(n, v, color="b", linestyle="-", linewidth=1.0, label='v')
-plt.legend()
-plt.xlabel('n')
-plt.savefig(path+'u_p_v.png')
-plt.show()
-
-
-
-n = 5
-model = BasisParameter(n,cfg,path)
 F,f, Epsilon_F,_,_,Epsilon_F_onevalue = model.consumer_theta()
+
+name =['theta','F','f','Epsilon_F','consumer_theta.png']
+model.visulation(name,F,f,Epsilon_F)
+
 H,h,Epsilon_H,_,_,Epsilon_H_onevalue = model.producer_phi()
-u, p, V, v,lambda_n,Epsilon_V, V_onevalue,v_onevalue, Epsilon_V_onevalue,lambda_n_onevalue = model.profit_Epsilon()
-plat_profit,_,_ = model.platform_profit(F)
+name =['phi','H','h','Epsilon_H','producer_phi.png']
+model.visulation(name,H,h,Epsilon_H)
+
+v,lambda_n,Epsilon_V, V_onevalue,v_onevalue, Epsilon_V_onevalue,lambda_n_onevalue = model.profit_Epsilon(p,V)
+name = ['n','V','v','profit_V.png']
+model.visulation2(name,V,v)
+name =['n','Epsilon_V','lambda_n','Epsilon_V+lambda_n.png']
+model.visulation2(name,Epsilon_V,lambda_n)
+
+plat_profit,plat_profit_dif_n,plat_profit_dif_theta = model.platform_profit(F)
 model.platform_profit_show_in3D()
-model.platform_lambda(Epsilon_H,Epsilon_F,Epsilon_V)
-model.producer_profit_to_constumer_profit(Epsilon_H,Epsilon_F,lambda_n,Epsilon_V)
+
+lambda_min,lambda_max=model.platform_lambda(Epsilon_H,Epsilon_F,Epsilon_V)
+producer_profit_to_constumer_profit= model.producer_profit_to_constumer_profit(Epsilon_H,Epsilon_F,lambda_n,Epsilon_V)
 
 model.producer_profit_to_constumer_profit(Epsilon_H_onevalue,Epsilon_F_onevalue,lambda_n_onevalue,Epsilon_V_onevalue)
 
-#print(F)
 
+print('u=',u)
+print('p=',p)
+print('V=',V)
+print('v=',v)
+print('coustomer_theta F=', F)
+print('coustomer_theta f=', f)
+print('coustomer_theta Epsilon_F=', Epsilon_F)
+
+print('producer_phi H=', H)
+print('producer_phi h=', h)
+print('producer_phi Epsilon_H=', Epsilon_H)
+
+print('profit_Epsilon Epsilon_V=', Epsilon_V)
+print('profit_Epsilon lambda_n=', lambda_n)
+
+print('plat_profit = ', plat_profit)
+print('plat_profit_dif_n =', plat_profit_dif_n)
+print('plat_profit_dif_theta=', plat_profit_dif_theta)
+print('producer_profit_to_constumer_profit',producer_profit_to_constumer_profit)
+print('lambda_min=', lambda_min)
+print('lambda_max =', lambda_max)
 
 ## plot producer theta
 
--- two_side.py
+++ two_side.py
@@ -18,11 +18,11 @@ class BasisParameter(object):
     define several parameter
     '''
 
-    def __init__(self,n, cfg2,path):
+    def __init__(self, cfg2,path):
         '''
         path--save the image
         '''
-        self.n = n
+
         self.cfg = cfg2
         self.path = path
         #print(self.cfg)
@@ -32,50 +32,24 @@ class BasisParameter(object):
         define the inflection point n=10
 
         '''
-
-        if (self.n > self.cfg.INFLECTION_POINT):
-            u = self.cfg.A + self.cfg.B * self.cfg.INFLECTION_POINT
-        else:
-            u = self.cfg.A + self.cfg.B * self.n
-
-
+        n = symbols('n')
+        u = self.cfg.A + self.cfg.B * n
         #self.epsilon = theta *self.f/self.F
         return u
+
     def consumer_theta(self):
 
         theta = symbols("theta")
         F = 1/(1 + exp(-(theta-5)))
         f = diff(F, theta)
         Epsilon_F = theta*f/F
-        print(type(F))
-
-        ## show the plot
-        theta =[]
-        F_value =[]
-        f_value = []
-        Epsilon_F_value = []
-        for i in np.arange(0,10,0.1):
-            theta.append(i)
-            F_value.append(float(F.subs('theta',i)))
-            f_value.append(float(f.subs('theta', i)))
-            Epsilon_F_value.append(float(Epsilon_F.subs('theta',i)))
-
-        plt.plot(theta, F_value, "r-", linewidth=1, label='F='+str(F))  # 画图
-        plt.plot(theta, f_value, "y-", linewidth=1, label='f='+str(f))  # 画图
-        plt.plot(theta, Epsilon_F_value, "g-", linewidth=1, label='Epsilon_F='+str(Epsilon_F))  # 画图
-        plt.legend()
-        plt.xlabel('theta')
-        plt.savefig(self.path+'consumer_theta.png')
-        plt.show()
 
         #calculate the value
         F_onevalue = float(F.subs('theta', self.cfg.THETA))
         f_onevalue = float(f.subs('theta', self.cfg.THETA))
         Epsilon_F_onevalue = self.cfg.THETA*f_onevalue/F_onevalue
         #print(F_onevalue)
-        print('coustomer_theta F=',F)
-        print('coustomer_theta f=', f)
-        print('coustomer_theta Epsilon_F=', Epsilon_F)
+
         return F,f,Epsilon_F, F_onevalue, f_onevalue, Epsilon_F_onevalue
 
     def producer(self):
@@ -83,11 +57,10 @@ class BasisParameter(object):
         self.p = the profit per platform consumer net of variable costs
 
         '''
-        if (self.n >self.cfg.INFLECTION_POINT):
-            p = self.cfg.C - self.cfg.D * self.cfg.INFLECTION_POINT
-        else:
-            p = self.cfg.C - self.cfg.D * self.n
+        n = symbols('n')
+        p = self.cfg.C - self.cfg.D * n
         return p
+
     def producer_phi(self):
 
         phi = symbols("phi")
@@ -95,83 +68,27 @@ class BasisParameter(object):
         h = diff(H, phi)
         Epsilon_H = phi*h/H
 
-        ## show the plot
-        phi =[]
-        H_value =[]
-        h_value = []
-        Epsilon_H_value = []
-        for i in np.arange(0,10,0.1):
-            phi.append(i)
-            H_value.append(float(H.subs('phi',i)))
-            h_value.append(float(h.subs('phi', i)))
-            Epsilon_H_value.append(float(Epsilon_H.subs('phi',i)))
-
-        plt.plot(phi, H_value, "r-", linewidth=1, label='H='+str(H))  # 画图
-        plt.plot(phi, h_value, "y-", linewidth=1, label='h='+str(h))  # 画图
-        plt.plot(phi, Epsilon_H_value, "g-", linewidth=1, label='Epsilon_H='+str(Epsilon_H))  # 画图
-        plt.legend()
-        plt.xlabel('phi')
-        plt.savefig(self.path+'producer_phi.png')
-        plt.show()
-
         #calculate the value
         H_onevalue = float(H.subs('phi', self.cfg.PHI))
         h_onevalue = float(h.subs('phi', self.cfg.PHI))
         Epsilon_H_onevalue = self.cfg.PHI*h_onevalue/H_onevalue
         #print(F_onevalue)
-        print('producer_phi H=', H)
-        print('producer_phi h=', h)
-        print('producer_phi Epsilon_H=', Epsilon_H)
+
         return H,h,Epsilon_H, H_onevalue, h_onevalue, Epsilon_H_onevalue
 
     def profit(self,u,p):
         '''
         self.V = denote the gross surplus created by n products for each platform consumer
         '''
-        V = u + p * self.n
+        n = symbols('n')
+        V = u + p *n
         return V
-    def profit_Epsilon(self):
+    def profit_Epsilon(self,p,V):
 
         n = symbols("n")
-        u = self.cfg.A + self.cfg.B * n
-        p = self.cfg.C - self.cfg.D *n
-        V = u + p*n ## n<=10
         v = diff(V, n)
-        print('p(n)= ', p)
-        print('u(n)= ', u)
-        print('V(n)= ', V)
-        print('v(n)= ', v)
-
         Epsilon_V = n * v / V
         lambda_n = p /v
-        print('profit_Epsilon Epsilon_V=',Epsilon_V)
-        print('profit_Epsilon lambda_n=', lambda_n)
-
-        ## show the plot
-        n = []
-        V_value = []
-        v_value = []
-        Epsilon_V_value = []
-        lambda_n_value =[]
-        for i in np.arange(0, 10, 0.1):
-            n.append(i)
-            V_value.append(float(V.subs('n', i)))
-            v_value.append(float(v.subs('n', i)))
-            Epsilon_V_value.append(float(Epsilon_V.subs('n', i)))
-            lambda_n_value.append(float(lambda_n.subs('n',i)))
-
-        plt.plot(n, V_value, "r-", linewidth=1, label='V ='+str(V))
-        plt.plot(n, v_value, "y-", linewidth=1, label='v='+str(v))
-        plt.legend()
-        plt.xlabel('n')
-        plt.savefig(self.path+'profit_V.png')
-        plt.show()
-        plt.plot(n, Epsilon_V_value, "g-", linewidth=1, label='Epsilon_V='+str(Epsilon_V))
-        plt.plot(n, lambda_n_value, "b-", linewidth=1, label='lambda_n='+str(lambda_n))
-        plt.xlabel('n')
-        plt.legend()
-        plt.savefig(self.path + 'Epsilon_V+lambda_n.png')
-        plt.show()
 
         # calculate the value
         V_onevalue = float(V.subs('n', self.cfg.N))
@@ -179,7 +96,7 @@ class BasisParameter(object):
         Epsilon_V_onevalue = self.cfg.PHI * v_onevalue / V_onevalue
         lambda_n_onevalue = float(lambda_n.subs('n',self.cfg.N))
         # print(F_onevalue)
-        return u,p,V,v, lambda_n,Epsilon_V, V_onevalue, v_onevalue, Epsilon_V_onevalue,lambda_n_onevalue
+        return v, lambda_n,Epsilon_V, V_onevalue, v_onevalue, Epsilon_V_onevalue,lambda_n_onevalue
 
     def platform_profit(self,F):
         n, theta= symbols("n theta")
@@ -188,9 +105,6 @@ class BasisParameter(object):
         plat_profit_dif_n = diff(plat_profit,n)
         plat_profit_dif_theta = diff(plat_profit,theta)
 
-        print('plat_profit = ',plat_profit)
-        print('plat_profit_dif_n =',plat_profit_dif_n)
-        print('plat_profit_dif_theta=',plat_profit_dif_theta)
         theta = []
         n=[]
 
@@ -221,21 +135,7 @@ class BasisParameter(object):
         producer_profit_to_constumer_profit = Epsilon_V*(1+Epsilon_F)*(1-(1-lambda_n)*(1+Epsilon_H))/ \
                                               (1+Epsilon_H)*(1-lambda_n*Epsilon_V*(1+Epsilon_F))
 
-
-        print(type(producer_profit_to_constumer_profit))
-
-        print(producer_profit_to_constumer_profit)
         return(producer_profit_to_constumer_profit)
-
-    def calculate(self,producer_profit_to_constomer_profit):
-
-        n=10
-        phi=5
-        theta=5
-
-
-
-
 
     def platform_profit_show_in3D(self):
         '''
@@ -265,10 +165,6 @@ class BasisParameter(object):
         lambda_max = 1/Eplison_V * (1 + Epsilon_F)
         lambda_min = Epsilon_H / (1 + Epsilon_H)
 
-        lambda_max_value = []
-        lambda_min_value = []
-        print('lambda_min=', lambda_min)
-        print('lambda_max =',lambda_max)
         '''
         theta = []
         phi=[]
@@ -289,14 +185,45 @@ class BasisParameter(object):
         plt.savefig(self.path + 'platform_lambda.png')
         plt.show()
         '''
+        return lambda_min,lambda_max
 
+    def visulation(self,args,x1,x2,x3):
 
+        x1_value = []
+        x2_value = []
+        x3_value = []
+        x_value= []
+        for i in np.arange(0, 10, 0.1):
+            x_value.append(i)
+            x1_value.append(float(x1.subs(args[0], i)))
+            x2_value.append(float(x2.subs(args[0], i)))
+            x3_value.append(float(x3.subs(args[0], i)))
 
-
-    def visualisieren(self,n,p):
-        print('#######')
         plt.figure()
-        plt.plot(n, p)
+        plt.plot(x_value, x1_value, "r-", linewidth=1, label=args[1]+'='+str(x1))  # 画图
+        plt.plot(x_value, x2_value, "y-", linewidth=1, label=args[2]+'='+str(x2))  # 画图
+        plt.plot(x_value, x3_value, "g-", linewidth=1, label=args[3]+'='+str(x3))  # 画图
+        plt.legend()
+        plt.xlabel(args[0])
+        plt.savefig(self.path + args[4])
+        plt.show()
+
+    def visulation2(self,args,x1,x2):
+
+        x1_value = []
+        x2_value = []
+        x_value= []
+        for i in np.arange(0, 10, 0.1):
+            x_value.append(i)
+            x1_value.append(float(x1.subs(args[0], i)))
+            x2_value.append(float(x2.subs(args[0], i)))
+
+        plt.figure()
+        plt.plot(x_value, x1_value, "r-", linewidth=1, label=args[1]+'='+str(x1))  # 画图
+        plt.plot(x_value, x2_value, "y-", linewidth=1, label=args[2]+'='+str(x2))  # 画图
+        plt.legend()
+        plt.xlabel(args[0])
+        plt.savefig(self.path + args[3])
         plt.show()
 
 
